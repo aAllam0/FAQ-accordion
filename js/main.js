1637601c5df95b0e07e23a9bd08967070acedb11
Abdallah-Allam <132194354+Abdallah-Allam@users.noreply.github.com>
2024-10-07 10:17:32 +0300

    Upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const faqsRows = document.querySelectorAll(".faqs .content .faqs-row");
+const questions = document.querySelectorAll(
+  ".faqs .content .faqs-row .question"
+);
+
+console.log(faqsRows);
+console.log(questions);
+
+questions.forEach((question) => {
+  question.addEventListener("click", (e) => {
+    if (e.currentTarget.parentElement.classList.contains("active")) {
+      faqsRows.forEach((row) => row.classList.remove("active"));
+    } else {
+      faqsRows.forEach((row) => row.classList.remove("active"));
+      e.currentTarget.parentElement.classList.add("active");
+    }
+  });
+});
